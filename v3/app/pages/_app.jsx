6ae369f7437beaf518894f7f1a4d55c9611b9fda
Borek Bernard <borekb@gmail.com>
2021-05-22 13:02:10 +0200

    Delete old versions

--- v3/app/pages/_app.jsx
+++
@@ -1,7 +0,0 @@
-import '../styles/normalize.css'
-import '../styles/style.css'
-import '../styles/custom.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
